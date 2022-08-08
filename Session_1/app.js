59f3870d49b29d67e3ae8aa6ea6b7e4ace3f1d3d
adem kalfalı <ademkalfa9@gmail.com>
2022-08-08 07:49:33 +0300

    console.log_aler_let_

--- Session_1/app.js
+++ Session_1/app.js
@@ -1,1 +1,11 @@
-console.log('app.js is running')
+console.log('app.js is running');
+// alert('Hello World');
+// alert('Hello World');
+// alert('Hello World');
+// window.alert('cw')
+
+// let firstname = prompt('Enter your name:');
+// alert(firstName + ' ' + firstName)
+
+let sonuc = confirm('are you legal?');
+console.log(sonuc);
