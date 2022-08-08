efaeaa0f77cd5247e1f33b6e25dc0b1f0d8a4406
adem kalfalı <ademkalfa9@gmail.com>
2022-08-08 20:00:28 +0300

    var-let study

--- Session_1/app.js
+++ Session_1/app.js
@@ -7,5 +7,44 @@ console.log('app.js is running');
 // let firstname = prompt('Enter your name:');
 // alert(firstName + ' ' + firstName)
 
-let sonuc = confirm('are you legal?');
-console.log(sonuc);
+// let sonuc = confirm('are you legal?');
+// console.log(sonuc);
+
+// var myNumber1 = 15;
+// console.log(myNumber1);
+
+/*
+var ile değer atandığında aynı nesneye tekrar 
+başka değer atanırsa var o nesnenin yeni bellek 
+adresi atanmasına izin verir.
+*/
+
+// var myNumber1 = 22 ;
+// console.log(myNumber1);
+
+// var myNumber1 = 11;
+// console.log(myNumber1);
+// let v1 = 55;
+// console.log(v1);
+
+
+
+// var myNumber1 = 11;
+// console.log(myNumber1);
+// let v1 = 88;
+// console.log(v2);
+
+// const
+
+const student = {
+    name: 'Adem',
+    age: 30,
+};
+
+console.log(student);
+
+const otherstudent = student;
+
+otherstudent.name = 'Dan';
+console.log(student);
+console.log(otherstudent);
