b4f33884f11f965bf51a5f6668893d9a1776cc9f
adem kalfalı <ademkalfa9@gmail.com>
2022-08-09 21:03:08 +0300

    typeof-Data Types

---
+++ Session-2/app.js
@@ -0,0 +1,35 @@
+console.log('App js Running')
+
+// let a = 5;
+// console.log(a);
+
+// {
+//     let a=3;
+//     console.log(a);
+// }
+
+// a = 6;
+// console.log(a);
+
+// console.log(typeof 0);
+// console.log(typeof 3.14);
+// console.log(typeof 'hello');
+// console.log(typeof (8 > 10));
+// console.log((8 > 10));
+// console.log(typeof ' ');
+// console.log(typeof '');
+
+// let b = prompt('Enter your name');
+
+// console.log(b, typeof b)
+
+// console.log(typeof typeof 42)
+
+let age;
+
+console.log(age);
+
+
+console.log(myAge);
+var myAge = 40;
+console.log('Hello');
