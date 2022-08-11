dbf027ab6dc16182b6966787a7d50ca0662e4e28
adem kalfalı <ademkalfa9@gmail.com>
2022-08-11 23:42:38 +0300

    Operators-promp

---
+++ Session-3/app.js
@@ -0,0 +1,62 @@
+console.log('App js Running')
+
+// let selamlama = `Hoşgeldiniz ${username}, xyz Bankası iyi günler diler!`;
+
+// userName = null;
+
+// selamlama
+// `Hoşgeldiniz undefined, xyz Bankası iyi günler diler!`
+// let user2 =(userName == undefined) && 'Müşterimiz';
+
+// selamlama2
+// `Hoşgeldiniz Müşterimiz, XYZ Bankası iyi günler diler!`
+
+// num1 = prompt('Enter num1: ');
+// num2 = prompt('Enter num2: ');
+
+// console.log(`${num1}*${num2}=${num1*num2}, ${num1}/${num2}=${num1/num2}`);
+
+// area of ​​rectangle
+
+// let side1 = +prompt('Enter first side: ');
+// let side2 = +prompt('Enter second side: ');
+
+// console.log(`Area of Rentangle=${side1*side2}`);
+
+//  Daire Yarıçap
+// let side3 = prompt('Enter first side: ');
+
+// console.log(3.14*(side3*side3));
+
+
+// let F = +prompt('Enter first side: ');
+
+// console.log((5*(F-32))/9);
+
+//  let C = prompt('Enter first side: ');
+//  console.log(`${C} (9*C+(32*5))/5`);
+
+
+// const today = new Date();
+// console.log(today);
+
+// console.log(`Yıl ${today.getFullYear()}`);
+// console.log(`Ay ${today.getMonth()+1}`);
+// console.log(`Gün ${today.getDate()}`);
+
+// console.log(`Day of the month is ${now.getDate()}`);
+// console.log(`Month is ${now.getMonth()}`);
+// console.log(`Year is ${now.getFullYear()}`);
+
+// let num8 = +prompt('Enter first num: ')%10;
+// let num9 = +prompt('Enter second num: ')%10;
+// let num10 = +prompt('Enter third num: ')%10;
+
+// console.log(`3 sayıdan en az ikisinin birler basamağı eşittir: ${(num8==num9)||(num8==num10)||(num9==num10)}`)
+
+
+// let num1=+prompt(`first number`)%10;
+// let num2=+prompt(`second number`)%10;
+// let num3=+prompt(`third number`)%10;
+
+// console.log((num1==num2)||(num1==num3)||(num2==num3))
