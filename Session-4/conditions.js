ca61782226f4081058f9e8c1637874c49d021fd5
adem kalfalı <ademkalfa9@gmail.com>
2022-08-16 07:33:00 +0300

    if-else if

--- Session-4/conditions.js
+++ Session-4/conditions.js
@@ -4,14 +4,28 @@ console.log("*****CONDITIONS*****")
 
 //?GİRİLEN SAYI POZİTİF NEGATİF VEYA SIFIR OLDUĞU
 
-const sayi = Number(prompt("Bir sayı giriniz:"));
 // const sayi = Number(prompt("Bir sayı giriniz:"));
-if(sayi > 0 ){
-    console.log(`${sayi} pozitiftir.`);
-}else if(sayi == 0){
-    console.log(`${sayi} 0'a esittir.`);
-}else{
-    console.log(`${sayi} negatiftir`);
-}
+// // const sayi = Number(prompt("Bir sayı giriniz:"));
+// if(sayi > 0 ){
+//     console.log(`${sayi} pozitiftir.`);
+// }else if(sayi == 0){
+//     console.log(`${sayi} 0'a esittir.`);
+// }else{
+//     console.log(`${sayi} negatiftir`);
+// }
 
-console.log(sayi, typeof sayi);//default olarak her girilen veri HTML tasarımı nedeniyle string olarak kabul edilir. promp önüne number veya + konularak sayı olarak nitelendirilir.
+// console.log(sayi, typeof sayi);//default olarak her girilen veri HTML tasarımı nedeniyle string olarak kabul edilir. promp önüne number veya + konularak sayı olarak nitelendirilir.
+
+//? console dan 3 tane tamsayi alarak en büyüğünü bulan
+
+const n1 = prompt("sayi1:")
+const n2 = prompt("sayi2:")
+const n3 = prompt("sayi3:")
+
+if(n1>=n2 && n1>=n3){
+    console.log(`${n1} en buyuk sayidir.`);
+}else if(n2>=n1 && n2>=n3){
+    console.log(`${n2} en buyuk sayidir.`);
+}else if(n3>=n1 && n3>=n2){
+    console.log(`${n3} en buyuk sayidir.`);
+}
