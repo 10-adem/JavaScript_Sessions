9957ae3ef2ef3bdb0387b4ee9b1936b1c7f7b8eb
adem kalfalı <ademkalfa9@gmail.com>
2022-08-16 07:20:35 +0300

    Number

--- Session-4/conditions.js
+++ Session-4/conditions.js
@@ -4,8 +4,8 @@ console.log("*****CONDITIONS*****")
 
 //?GİRİLEN SAYI POZİTİF NEGATİF VEYA SIFIR OLDUĞU
 
-const sayi = prompt("Bir sayı giriniz:")
-
+const sayi = Number(prompt("Bir sayı giriniz:"));
+// const sayi = Number(prompt("Bir sayı giriniz:"));
 if(sayi > 0 ){
     console.log(`${sayi} pozitiftir.`);
 }else if(sayi == 0){
@@ -13,3 +13,5 @@ if(sayi > 0 ){
 }else{
     console.log(`${sayi} negatiftir`);
 }
+
+console.log(sayi, typeof sayi);//default olarak her girilen veri HTML tasarımı nedeniyle string olarak kabul edilir. promp önüne number veya + konularak sayı olarak nitelendirilir.
