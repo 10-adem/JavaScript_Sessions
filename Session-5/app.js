becb1b26bb097798c6380c528a779a933fe05421
adem kalfalı <ademkalfa9@gmail.com>
2022-08-17 22:56:29 +0300

    prediction

--- Session-5/app.js
+++ Session-5/app.js
@@ -68,3 +68,12 @@ console.log("*****FOR*****");
 
 // console.log("You entered number:", number);
 
+
+//* Program 0*100 arasında rasgele bir sayı tutacak ve kullanıcının bu sayıyı 5 kere(hak) de bilmesini isteyecektir. Her yanlışta hakkını bir düşürecek ve ARTTIR/AZALT diyerek kullanıcıyı yönlendirecektir.Sonuç olarak kullanıcının hakkı 0 olursa "Üzgünüz bilemediniz" eğer bildi ise "Tebrikler ... denemede bildiniz" yazacaktır.
+
+let hak = 5;
+const rastgele = Math.round(Math.random()*100);
+console.log(rastgele);
+do{
+    const tahmin = +prompt("Lutfen 0-100 arasında bir tahmin giriniz")
+}while();
