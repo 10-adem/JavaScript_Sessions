3c7875f722cc7dbfa25f93f825480a2b252ef235
adem kalfalı <ademkalfa9@gmail.com>
2022-08-21 08:13:17 +0300

    Study

--- Session-6/app.js
+++ Session-6/app.js
@@ -1,10 +1,26 @@
 console.log("*****FUNC DECLARATION*****");
-yazdir();
+// yazdir();
 
-//!Fonksiyonun tanimlanmasi (declaration)
-function yazdir() {
-  console.log("merhaba");
+// //!Fonksiyonun tanimlanmasi (declaration)
+// function yazdir() {
+//   console.log("merhaba");
+// }
+
+// yazdir(); //!invoke, call, cagirma
+// yazdir();
+
+//!Default Parametre
+function selamla(ad, soyad = "") {
+  console.log(`Merhaba ${ad} ${soyad}`);
 }
 
-yazdir(); //!invoke, call, cagirma
-yazdir();
+selamla("Can", "Yilmaz");
+selamla("Canan", "Ozturk");
+selamla("Ayşe");
+function yasHesapla(isim, dogumTarihi) {
+  const sonuc = `${isim} in yası ${new Date().getFullYear() - dogumTarihi} dir`;
+  return sonuc;
+}
+
+const mesaj1 = yasHesapla("Elif Can", 1990);
+console.log(mesaj1);
