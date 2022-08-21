43f7d3c7acb65384ded461af8d22a4d51ccb2441
adem kalfalı <ademkalfa9@gmail.com>
2022-08-21 14:08:12 +0300

    Calculator

--- Session-6/app.js
+++ Session-6/app.js
@@ -60,8 +60,75 @@ console.log(yasHesapla("Veli Canan", 1980));
 //   return sayi % 2 ? ` ${sayi}"TEKTİR"` : ` ${sayi}"CİFTTİR"`;
 // }
 
+//! Cannot access 'tekCift1' before initialization
+// console.log(tekCift1(9));
+
 const tekCift1 = function tekCift(sayi) {
   return sayi % 2 ? ` ${sayi}"TEKTİR"` : ` ${sayi}"CİFTTİR"`;
 };
 console.log(tekCift1(5));
 console.log(tekCift1(2));
+
+//
+
+const buyukBul = function (n1, n2, n3 = -Number.MAX_VALUE) {
+  let enBuyuk;
+
+  if (n1 >= n2 && n1 >= n3) {
+    enBuyuk = n1;
+  } else if (n2 >= n1 && n2 >= n3) {
+    enBuyuk = n2;
+  } else if (n3 >= n1 && n3 >= n2) {
+    enBuyuk = n3;
+  }
+
+  return enBuyuk;
+};
+
+// const n1 = +prompt("Sayi1:");
+// const n2 = +prompt("Sayi2:");
+// const n3 = +prompt("Sayi3:");
+
+// console.log(`Girilen sayilerin en buyugu: ${buyukBul(n1, n2, n3)}`);
+console.log(`Girilen sayilerin en buyugu: ${buyukBul(-8, -9, -1)}`);
+console.log(`Girilen sayilerin en buyugu: ${buyukBul(-8, -9)}`);
+
+const topla = function (s1, s2) {
+  return s1 + s2;
+};
+const cikar = function (s1, s2) {
+  return s1 - s2;
+};
+const carp = function (s1, s2) {
+  return s1 * s2;
+};
+const bol = function (s1, s2) {
+  return s1 / s2;
+};
+
+const hesapla = function (s1, s2, islem) {
+  let sonuc = 0;
+  switch (islem) {
+    case "+":
+      sonuc = topla(s1, s2);
+      break;
+
+    case "-":
+      sonuc = cıkar(s1, s2);
+      break;
+    case "/":
+      sonuc = bol(s1, s2);
+      break;
+    case "*":
+      sonuc = topla(s1, s2);
+      break;
+    default:
+      break;
+  }
+  return sonuc;
+};
+// console.log(hesapla(3, 5, "+"));
+// console.log(hesapla(4, 3, "/"));
+
+console.log(hesapla(3, 5, "+"));
+console.log(hesapla(4, 3, "/"));
