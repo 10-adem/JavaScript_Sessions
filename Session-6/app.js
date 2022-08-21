b9a827c454474e85bf7e5dca10f209652f2b9ef5
adem kalfalı <ademkalfa9@gmail.com>
2022-08-21 07:31:55 +0300

    Function declaration

--- Session-6/app.js
+++ Session-6/app.js
@@ -1,1 +1,10 @@
 console.log("*****FUNC DECLARATION*****");
+yazdir();
+
+//!Fonksiyonun tanimlanmasi (declaration)
+function yazdir() {
+  console.log("merhaba");
+}
+
+yazdir(); //!invoke, call, cagirma
+yazdir();
