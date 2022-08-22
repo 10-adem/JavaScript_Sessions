acbe56442ae96fb57f03edd5016d34d7e3e95231
adem kalfalı <ademkalfa9@gmail.com>
2022-08-22 22:36:23 +0300

    RECURSION

--- Session-7/app.js
+++ Session-7/app.js
@@ -29,27 +29,45 @@
 //* hesaplayarak yazdiran fonksiyonu dongu ile kodlayiniz.
 //? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, ...
 
-let stringResult = "1,1";
+// let stringResult = "1,1";
 //f(n) = f(n-1) + f(n-2) matematiksel yöntemlerle çözümü
 
-const fibo = (n) => {
-  let fib1 = 1;
-  let fib2 = 1;
-  let toplam = 0;
+// const fibo = (n) => {
+//   let fib1 = 1;
+//   let fib2 = 1;
+//   let toplam = 0;
 
-  for (let i = 2; i < n; i++) {
-    toplam = fib1 + fib2;
-    fib1 = fib2;
-    fib2 = toplam;
-    stringResult += "," + fib2;
+//   for (let i = 2; i < n; i++) {
+//     toplam = fib1 + fib2;
+//     fib1 = fib2;
+//     fib2 = toplam;
+//     stringResult += "," + fib2;
+//   }
+//   return fib2;
+// };
+
+// const n = Number(prompt("n terimini giriniz:"));
+// if (n <= 0) {
+//   console.log("Lutfen 0 dan buyuk bir sayi giriniz.");
+// } else {
+//   console.log(`FIBO(${n}) = ${fibo(n)} `);
+//   console.log(stringResult);
+// }
+
+//* ----------RECURSION-----------
+//*f(n) = f(n-1) + f(n-2)
+
+const fibo = (n) => {
+  if (n <= 0) {
+    return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    return fibo(n - 1) + fibo(n - 2);
   }
-  return fib2;
 };
 
 const n = Number(prompt("n terimini giriniz:"));
-if (n <= 0) {
-  console.log("Lutfen 0 dan buyuk bir sayi giriniz.");
-} else {
-  console.log(`FIBO(${n}) = ${fibo(n)} `);
-  console.log(stringResult);
-}
+
+console.log(`FIBO(${n}) = ${fibo(n)} `);
+//   console.log(stringResult);
