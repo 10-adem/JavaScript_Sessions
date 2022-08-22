c97752a00f987b6a4c416eb5d785576256b4c88c
adem kalfalı <ademkalfa9@gmail.com>
2022-08-22 22:15:08 +0300

    Study

--- Session-7/app.js
+++ Session-7/app.js
@@ -13,7 +13,7 @@
 
 //*ORNEK: YAS HESAPLAMA
 
-const tarih = Number(prompt("Dogum tarihini giriniz:"));
+// const tarih = Number(prompt("Dogum tarihini giriniz:"));
 // const yasHesapla = (tarih) => {
 //   const yas = new Date().getFullYear() - tarih;
 //   return yas;
@@ -21,10 +21,35 @@ const tarih = Number(prompt("Dogum tarihini giriniz:"));
 
 // console.log("Yasiniz:" + yasHesapla(tarih));
 
+// const yasHesaplaKisa = (tarih) => new Date().getFullYear() - tarih;
+
+// console.log("YASINIZ:" + yasHesaplaKisa(tarih));
+
 //* ORNEK: Girilen n. terime kadar Fibonacci sayilarinin toplamini
 //* hesaplayarak yazdiran fonksiyonu dongu ile kodlayiniz.
-//? FIBONACCI terimleri:  0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+//? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, ...
 
-const yasHesaplaKisa = (tarih) => new Date().getFullYear() - tarih;
+let stringResult = "1,1";
+//f(n) = f(n-1) + f(n-2) matematiksel yöntemlerle çözümü
 
-console.log("YASINIZ:" + yasHesaplaKisa(tarih));
+const fibo = (n) => {
+  let fib1 = 1;
+  let fib2 = 1;
+  let toplam = 0;
+
+  for (let i = 2; i < n; i++) {
+    toplam = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = toplam;
+    stringResult += "," + fib2;
+  }
+  return fib2;
+};
+
+const n = Number(prompt("n terimini giriniz:"));
+if (n <= 0) {
+  console.log("Lutfen 0 dan buyuk bir sayi giriniz.");
+} else {
+  console.log(`FIBO(${n}) = ${fibo(n)} `);
+  console.log(stringResult);
+}
