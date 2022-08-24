d56ff85065587f4bfc09cbcf7d9e7e9f0f70c586
adem kalfalı <ademkalfa9@gmail.com>
2022-08-24 22:21:32 +0300

    Study

--- Session-8/app.js
+++ Session-8/app.js
@@ -1,7 +1,7 @@
 console.log("*****STRING METHODS*****");
 
 //!Klasik yöntem (String Literals) ile string tanimlanirsa bu string bir primitive degiskendir.
-const str1 = "clarusway";
+let str1 = "clarusway";
 
 const str2 = "Hello";
 const str3 = "Full Stack";
@@ -17,3 +17,72 @@ const str5 = str1 + str2 + str3; // Concatination
 console.log(str5);
 
 //* concat() - imutable
+console.log(str1.concat(str2));
+console.log(str1, str2);
+str1 = str1.concat(str3);
+console.log(str1);
+
+//* toUpperCase(), toLowerCase()
+
+const myName = "Adam Wolf";
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+
+let yourName = "ismet";
+yourName = yourName.toLocaleUpperCase();
+console.log(yourName);
+
+//* 2 string esit veya degil diye kiyaslayarak sonucu
+//* donduren fonksiyonu yaziniz.
+
+// const esitMi = (str1, str2) => {
+//   return str1.toLocaleUpperCase() === str2.toLocaleUpperCase()
+//     ? `${str1} ile ${str2} Eşittir.`
+//     : `${str1} ile ${str2} Eşit Değildir.`;
+// };
+
+const esitMi = (str1, str2) =>
+  str1.toLocaleUpperCase() === str2.toLocaleUpperCase()
+    ? `${str1} ile ${str2} Eşittir.`
+    : `${str1} ile ${str2} Eşit Değildir.`;
+
+console.log(esitMi("MerHaba", "MERHABA"));
+console.log("Hello", "Hell");
+
+//*LocaleCompare();
+
+const a = "reserve";
+const b = "RESERVE";
+
+console.log(a.localeCompare(b, "en", { sensitiy: "base" }));
+
+//? 'BASE:'
+//? 'ACCENT:'
+//? 'CASE:'
+
+//*-------------------
+//* charAt()
+//*-------------------
+const str6 = "Full Stack Path";
+console.log(str6.charAt(5)); //* S
+console.log(str6.charAt(4)); //* ""
+console.log(str6.chartAt()); //* F (ilk indistekini dondurur)
+console.log(str6.chartAt(14)); //* h
+
+//! Lenght bir property (objenin degiskenidir) dir ve strign' in
+//! karakter sayisini tutar.
+console.log(str6.length); //*27
+
+//*String son harfini ogrenmek istersek
+console.log(str6.charAt(str6.length - 1)); //* -26 => 27 eleman
+
+//*-----------------------------------
+//* includes() - case sensitive
+//*-----------------------------------
+
+const word = "To be or not to be, that is the question";
+
+console.log(word.includes("to be")); //? true
+console.log(word.includes("That")); //? False
+console.log(word.includes("That")); //? False
+console.log(word.includes("")); //? False
