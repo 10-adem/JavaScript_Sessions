914a9ea437c317b5f11ffcb5887c728e5670f88c
adem kalfalı <ademkalfa9@gmail.com>
2022-08-29 22:15:08 +0300

    Array Study

--- Session-10/Array.js
+++ Session-10/Array.js
@@ -42,3 +42,43 @@ console.log(isimler);
 //number dizisinin son elementinin değerini 1 arttirin
 numbers[numbers.length - 1]++;
 console.log(numbers);
+
+const yaslar = [22, 44, 55];
+const kisiler = ["Ahmet", "Can", 2022, 2023 - 1990, true, yaslar];
+console.log(kisiler);
+
+const yas55 = kisiler[5][2];
+console.log(yas55);
+
+kisiler[5][1]--;
+console.log(kisiler);
+
+console.log(yaslar);
+
+const arabalar = ["BMW", "Mercedes", "Fiat", "Anadol"];
+
+/*pop()*/
+
+const silinen = arabalar.pop();
+console.log(arabalar, silinen);
+
+const n = arabalar.push("Citroen");
+console.log(arabalar, n);
+
+const n1 = arabalar.unshift("Audi");
+console.log(arabalar, n1);
+
+const audi = arabalar.shift();
+console.log(arabalar, audi);
+
+// 1.parametre eklenecek indis numarasi
+// 2.parametre 0 ise araya ekleme, 1 ise üzerine yazma
+// 3.parametre yeni eklenecek veri
+arabalar.splice(1, 0, "passat");
+console.log(arabalar);
+
+arabalar.splice(1, 1, "Jaguar");
+console.log(arabalar);
+
+arabalar.reverse();
+console.log(arabalar);
