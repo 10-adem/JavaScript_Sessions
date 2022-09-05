bc60ab72c7cb242af404f9d91a2cb7f9903d88c2
adem kalfalı <ademkalfa9@gmail.com>
2022-09-05 21:00:27 +0300

    Object Study

--- Session-10/Object.js
+++ Session-10/Object.js
@@ -15,3 +15,14 @@ console.log("MODEL:", arabalar["model"]); //? Sguare bracket notation
 
 const key = "marka";
 console.log(arabalar[key]);
+
+//? Object Constructure
+function Personel(id, ad, maas) {
+  this.perId = id;
+  this.perAd = "Ad";
+  this.maas = 15000;
+  console.log(this);
+}
+
+const kisi1 = new Personel("1234567890", "Mustafa", 15000);
+console.log(kisi1);
