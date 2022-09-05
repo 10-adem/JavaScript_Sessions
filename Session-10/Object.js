49daa313cf2bb8f6e5c36423e3e762514aba1b40
adem kalfalı <ademkalfa9@gmail.com>
2022-09-05 20:50:46 +0300

    Object Key

--- Session-10/Object.js
+++ Session-10/Object.js
@@ -1,1 +1,17 @@
 console.log("******Object*****");
+
+const arabalar = new Object();
+
+arabalar.marka = "BMW";
+arabalar.motor = "1.3";
+arabalar.model = "2022";
+arabalar.lpg = true;
+
+console.log(arabalar);
+
+//! Read
+console.log(arabalar.lpg); //?.notation
+console.log("MODEL:", arabalar["model"]); //? Sguare bracket notation
+
+const key = "marka";
+console.log(arabalar[key]);
