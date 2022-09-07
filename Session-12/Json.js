198458205a7d0928bd9bc042d2fb5ca6c8e7e5e0
adem kalfalı <ademkalfa9@gmail.com>
2022-09-07 22:47:01 +0300

    rest study

--- Session-12/Json.js
+++ Session-12/Json.js
@@ -66,5 +66,50 @@ const car = {
   colors: ["blue", "purple"],
 };
 
-const { name, colors, model, engine } = car;
-console.log(name, model, engine, colors);
+// const { name, colors, model, engine } = car;
+// console.log(name, model, engine, colors);
+
+// const { car1, car2 } = car;
+// console.log(car1);
+
+//* function
+const getInfo = () => {
+  return {
+    id: 1,
+    productName: "Iphone",
+    price: 30000,
+  };
+};
+
+console.log(getInfo());
+const { productName, price } = getInfo();
+
+console.log("Product Name:", productName);
+console.log("Product Price:", price);
+
+const names = ["Ahmet", "Mehmet", "İsmet", "Saffer"];
+
+const mehmet = names[1]; //* indexing
+
+const [p1, p2, p4] = names;
+console.log(p1, p2, p4); //* names deki index sayısı ile eşdeğer olmalıdır. ve eşdeğer sayısını getirir.
+
+//*REST(...) Array lerde object lerde geri kalanları alır. Sıra önemli olmakla beraber ayrılan alınan elemanlar sıralı olmalı ve rest operatörleri sonda olmalıdır.
+
+//!1. Kullanım
+const autos = ["reno", "anadol", "mercedes", "ferrari", "bmw"];
+
+const [reno, anadol, ...restAutos] = autos;
+console.log(reno, anadol);
+console.log(restAutos);
+
+const personel = {
+  name: "Josh",
+  surname: "Adams",
+  job: "developer",
+  age: 30,
+};
+const { name, job, ...ageSurname } = personel;
+console.log(ageSurname);
+
+//* SPRED (...)
