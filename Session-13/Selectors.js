d14c639a23c0d07f2cf64fa574f8f1a7d895da94
adem kalfalı <ademkalfa9@gmail.com>
2022-09-10 13:00:53 +0300

    QuerySelector

--- Session-13/Selectors.js
+++ Session-13/Selectors.js
@@ -47,16 +47,48 @@ console.log(list[1].innerText);
 //? HTML kodlarini calitirir.(Güvenlik acisindan problemlidir)
 list[4].innerHTML = "<a href='https://www.clarusway.com'>Clarusway Web</a>";
 
-//*EXAMPLE-3
-//*-------------------------------------------
-
 //*===========================================
 //*          GETELEMENTSBYCLASSNAME()
 //*===========================================
 
 //* EXAMPLE-4
 //* ------------------------------------------
+const myList = document.getElementsByClassName("list");
+console.log(myList);
+console.log(myList[0].innerText);
+
+//?Dizi metıtlari dogrudan kullanilamaz.
+
+//?Array.from()
+const myListArray = Array.from(myList);
+myListArray.forEach((item) => console.log(item.innerText));
+
+//?spread
+[...myList].forEach((item) => console.log(item.innerText));
+[...myList].forEach((item) => (item.style.color = "red"));
 
 //* ========================================
 //*              QUERYSELECTOR()
 //* ========================================
+const myLi = document.querySelector("li");
+console.log(myLi.innerText);
+myLi.style.color = "yellowgreen";
+
+//? class seçme
+const itemList = document.querySelector(".item-list");
+itemList.style.background = "grey";
+
+//? id seçme
+
+const input = document.querySelector("#input");
+console.log(input.value);
+
+document.querySelector("#btn").style.cursor = "pointer";
+
+const myBtn = document.querySelector("input[type ='button']");
+console.log(myBtn);
+
+const lists = document.querySelectorAll("ul li");
+console.log(lists); //? nodelist
+
+lists.forEach((li) => console.log(li.innerText));
