3086209b57a4e2de5f89d474338f5f07afee5bd7
adem kalfalı <ademkalfa9@gmail.com>
2022-09-13 13:45:36 +0300

    Events Study

--- Session-14/Events.intro/events.js
+++ Session-14/Events.intro/events.js
@@ -10,6 +10,7 @@ console.log("****** EVENTS *******");
 const h1 = document.querySelector("#header h1");
 h1.onmouseover = function () {
   h1.style.color = "red";
+  h1.style.fontWeight = "300";
 };
 
 h1.onmouseout = () => {
