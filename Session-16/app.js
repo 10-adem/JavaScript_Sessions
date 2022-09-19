7f6e99c0c84fe8e693a159c208845b199f820d7d
adem kalfalı <ademkalfa9@gmail.com>
2022-09-19 22:03:03 +0300

    Todo_Project

---
+++ Session-16/app.js
@@ -0,0 +1,63 @@
+//* ======================================================
+//*                     TODO APP
+//* ======================================================
+
+//?Selectors
+const addBtn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+
+addBtn.addEventListener("click", () => {
+  if (todoInput.value.trim() === "") {
+    alert("Please enter new todo");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
+    createListElement(newTodo);
+    todoInput.value = "";
+  }
+});
+
+const createListElement = (newTodo) => {
+  const li = document.createElement("li");
+  // li.id = newTodo.id
+  li.setAttribute("id", newTodo.id);
+
+  //   newTodo.completed ? li.classList.add("completed") : "";
+  newTodo.completed && li.classList.add("checked");
+
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+
+  const p = document.createElement("p");
+  const pTextNode = document.createTextNode(newTodo.text);
+  p.appendChild(pTextNode);
+  li.appendChild(p);
+
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fas fa-trash");
+  li.appendChild(deleteIcon);
+  todoUl.appendChild(li);
+};
+
+todoUl.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.classList.contains("fa-trash")) {
+    e.target.parentElement.remove();
+  }
+});
+
+todoInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    addBtn.click();
+  }
+});
+
+//? Baslangicta input aktif olsun
+window.onload = function () {
+  todoInput.focus();
+};
