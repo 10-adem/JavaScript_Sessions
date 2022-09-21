c65465515363a7c9d4ac665e8caaabc17643e8ed
adem kalfalı <ademkalfa9@gmail.com>
2022-09-21 23:13:06 +0300

    Study

--- Session-17/app.js
+++ Session-17/app.js
@@ -5,6 +5,7 @@ const shippingPrice = 15;
 const shippingFreePrice = 300;
 
 window.addEventListener("load", () => {
+  calculateCartPrice();
   //set items to LocalStorage
   localStorage.setItem("taxRate", taxRate);
   localStorage.setItem("shippingPrice", shippingPrice);
@@ -65,11 +66,23 @@ const calculateCartPrice = () => {
   productTotalPricesDivs.forEach((div) => {
     subtotal += parseFloat(div.innerText);
   });
-  const taxPrice = subtotal * localStorage.getItem("texRate");
+  const taxPrice = subtotal * localStorage.getItem("taxRate");
 
-  const shippingPrice =
+  const shippingPrice = parseFloat(
     subtotal > 0 && subtotal < localStorage.getItem("shippingFreePrice")
       ? localStorage.getItem("shippingPrice")
-      : 0;
+      : 0
+  );
   console.log(shippingPrice);
+  document.querySelector("#cart-subtotal").lastElementChild.innerText =
+    subtotal.toFixed(2);
+  document.querySelector("#cart-tax p:nth-child(2)").innerText =
+    taxPrice.toFixed(2);
+  document.querySelector("#cart-shipping").children[1].innerText =
+    shippingPrice.toFixed(2);
+  document.querySelector("#cart-total").lastElementChild.innerText = (
+    subtotal +
+    taxPrice +
+    shippingPrice
+  ).toFixed(2);
 };
