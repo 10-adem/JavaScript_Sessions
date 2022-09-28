23bdb8ef4840dc1da471aed9c653760129212b88
adem kalfalı <ademkalfa9@gmail.com>
2022-09-28 20:20:35 +0300

    Session-20

---
+++ Session-20/weather.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("section.top-banner form");
+const input = document.querySelector(".container input");
+const msg = document.querySelector("span.msg");
+const list = document.querySelector(".ajax-section .cities");
+
+localStorage.setItem("tokenKey", "be93f1449e7436fbdf0edfe2283a0612");
