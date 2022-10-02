a0a5e12bd04dcb7eb6d12499eff644517314a957
adem kalfalı <ademkalfa9@gmail.com>
2022-10-02 15:03:05 +0300

    Study

--- Session-21/2-objectConstructor.js
+++ Session-21/2-objectConstructor.js
@@ -7,3 +7,32 @@
 //* ve metotlari bir prototipten miras olarak almaktadir.
 //* Object prototipi, prototip miras zincirinin en tepesinde yer almaktadir.
 //* Ornegin Date ve Array nesneleri Object.prototype'dan miras almaktadir.
+
+//?Object Constructor
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getSummary = function () {
+    return `${this.title} was written by ${this.author} is ${this.year}`;
+  };
+}
+
+//? new keyword'u Book Constructor'ini parameterler ile cagirmaktadir.
+//? Constructor ise Book nesnesinden bir ornek (instance) olusturmaktadir.
+//? Constructor, mantiksal bir ifade iken instance fiziksel bir olusum gibi dusunulebilir.
+//? Contructor'da tanimlanmis tum degisken ve fonksiyonlar olusturulan
+//? her bir instance'da hayat bulmus olur.
+
+//?instance
+const book1 = new Book("Kasagi", "Omer Seyfettin", 1920);
+
+//?instance
+const book2 = new Book("Sinekl Bakkal", "H.Edip Adıvar", 1930);
+
+console.log(book1);
+console.log(book1.getSummary());
+
+book1.price = 100;
+console.log(book1);
+console.log(book2);
