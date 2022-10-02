8133078c764575b59b9a469f0dc3811c98a66c0d
adem kalfalı <ademkalfa9@gmail.com>
2022-10-02 15:30:27 +0300

    Session-study

--- Session-21/2-objectConstructor.js
+++ Session-21/2-objectConstructor.js
@@ -30,6 +30,12 @@ const book1 = new Book("Kasagi", "Omer Seyfettin", 1920);
 //?instance
 const book2 = new Book("Sinekl Bakkal", "H.Edip Adıvar", 1930);
 
+//! Prototype, belirli bir Nesne'nin (Object) tum instance'larina
+//! kolay bir sekilde metotlar tanimlamaya izin vermektedir.
+//! Prototip alaninda bir metot tanimlamanin avantaji bu metot'un
+//! olusan tum instance'larin belleginde yer kaplamamasi ancak tum
+//! instance'larin bu metota ulasabilmesidir.
+
 Book.prototype.getAge = function () {
   return new Date().getFullYear() - this.year;
 };
@@ -38,12 +44,27 @@ Book.prototype.getSummary = function () {
   return `${this.title} was written by ${this.author} is ${this.year}`;
 };
 
+Book.prototype.price = 100;
+// book1.price = 100;
+
+//* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
+//* Ancak, getAge() fonksiyonu bellekte sadece bir yer kaplamaktadir.
+
+//* Bir nesnenin prototiplerine .prototype ile erisilebilir.
+//* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
+
+console.log(Book.prototype);
+console.log(book1.__proto__);
+
 console.log(book1);
 console.log(book1.getSummary());
 
-book1.price = 100;
+// book1.price = 100;
 console.log(book1, book2);
 
 console.log(book1, book2);
 console.log(book1.getAge());
 console.log(book2.getAge());
+
+//? INHERITANCE (Kalitim - ES5)
+//?----------------------------------------------------------
